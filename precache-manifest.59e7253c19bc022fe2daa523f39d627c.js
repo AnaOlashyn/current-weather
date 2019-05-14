498bf4ee320e48aeebb4ecc75252a7b55f5f31f6
AnaOlashyn <anaolashyn@gmail.com>
2019-05-14 15:06:15 +0300

    Updates

---
+++ precache-manifest.59e7253c19bc022fe2daa523f39d627c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/current-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "bfac95cd5adae0813e1b",
+    "url": "/current-weather/static/css/main.93ccbf5e.chunk.css"
+  },
+  {
+    "revision": "81b55f55d529907d443f",
+    "url": "/current-weather/static/js/runtime~main.712cc06d.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/current-weather/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "4bb5ae12d7fed59ea350478e32c1f590",
+    "url": "/current-weather/static/media/zont011.4bb5ae12.jpg"
+  },
+  {
+    "revision": "bfac95cd5adae0813e1b",
+    "url": "/current-weather/static/js/main.c209ded1.chunk.js"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/current-weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/current-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/current-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/current-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "9f39bbdd8795b38bc9d8e76f468720ff",
+    "url": "/current-weather/index.html"
+  }
+];
